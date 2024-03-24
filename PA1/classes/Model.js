1536d619f6618cf9370c908957934c4a4ff6722e
DmytroKrava <93785602+DmytroKrava@users.noreply.github.com>
2024-03-24 14:04:31 +0200

    Add files via upload

---
+++ PA1/classes/Model.js
@@ -0,0 +1,30 @@
+// Constructor
+class Model {
+    constructor(name) {
+        this.name = name;
+        this.iVertexBuffer = gl.createBuffer();
+        this.iVertexBufferOfNormal = gl.createBuffer();
+        this.iVertexBufferOfTexCoord = gl.createBuffer();
+        this.count = 0;
+    }
+
+    BufferData = function (vertices, normals, texCoords) {
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
+
+        this.count = vertices.length / 3;
+    }
+
+    Draw = function () {
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribVertex);
+
+        gl.uniform4fv(shProgram.iColor, [0, 0, 0, 1]);
+        gl.drawArrays(gl.LINE_STRIP, 0, this.count);
+        gl.uniform4fv(shProgram.iColor, [1, 1, 0, 1]);
+        gl.drawArrays(gl.TRIANGLES, 0, this.count);
+    }
+}
